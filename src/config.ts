3097d59cd3e0b9d351df7cd80846a62ab18c40c6
tunjan <trigo@inventati.org>
2024-03-20 11:18:30 +0100

    modified:   public/assets/logo.svg

--- src/config.ts
+++ src/config.ts
@@ -19,8 +19,8 @@ export const LOCALE = {
 export const LOGO_IMAGE = {
   enable: true,
   svg: true,
-  width: 216,
-  height: 46,
+  width: 50,
+  height: 100,
 };
 
 export const SOCIALS: SocialObjects = [
